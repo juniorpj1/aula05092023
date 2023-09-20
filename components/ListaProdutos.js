66cb6b880176832067e49d1cbb7e56a3caeee157
apariciojr <apariciojunior11@gmail.com>
2023-09-20 12:15:37 -0300

    annotations at class

--- components/ListaProdutos.js
+++ components/ListaProdutos.js
@@ -3,11 +3,12 @@ import RenderizaItem from "./RenderizaItem";
 import { DATA } from "../data/dados";
 import { useNavigation } from "@react-navigation/native";
 
+// rnfes - code snippets
 const ListaProdutos = () => {
   const navigation = useNavigation();
 
   return <FlatList numColumns={2} data={DATA} columnWrapperStyle={{ justifyContent: "space-between" }}
-    renderItem={({ item } ) => RenderizaItem(item, navigation)} />;
+    renderItem={({ item }) => RenderizaItem(item, navigation)} />;
 };
 
 export default ListaProdutos;
